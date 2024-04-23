d36c3f255f4c61e3799994f45a0f142902a21332
unknown <leonideko1@gmail.com>
2024-04-23 22:15:25 +0300

    add firebase to store items

--- src/App.js
+++ src/App.js
@@ -1,17 +1,63 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
+import { Fragment, useEffect, useState } from "react";
 
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import Notification from "./components/UI/Notification";
+import { sendCartData, fetchCartData } from "./store/cart-actions";
+
+let isInitial = true;
 
 function App() {
-  const showCart = useSelector(state=>state.ui.cartIsVisible)
+  const dispatch = useDispatch();
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+  const [notificationVisible, setNotificationVisible] = useState(false);
+
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
+  }, [cart, dispatch]);
+
+  useEffect(() => {
+    if (notification) {
+      setNotificationVisible(true);
+      const timer = setTimeout(() => {
+        setNotificationVisible(false);
+      }, 1000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [notification]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notificationVisible  && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -4,15 +4,28 @@ import classes from "./Products.module.css";
 const DUMMY_PRODUCTS = [
   {
     id: "p1",
-    price: 6,
-    title: "My First Book",
-    description: "The first book I ever wrote",
+    price: 10,
+    title: "Adventures in Wonderland",
+    description: "Join Alice in her adventures through Wonderland.",
   },
   {
     id: "p2",
-    price: 5,
-    title: "My Second Book",
-    description: "The Second book I ever wrote",
+    price: 12,
+    title: "The Great Gatsby",
+    description: "A tale of love, wealth, and the American Dream.",
+  },
+  {
+    id: "p3",
+    price: 8,
+    title: "To Kill a Mockingbird",
+    description: "A powerful story of racial injustice and moral growth.",
+  },
+  {
+    id: "p4",
+    price: 15,
+    title: "The Hitchhiker's Guide to the Galaxy",
+    description:
+      "Embark on a hilarious journey through space with Arthur Dent.",
   },
 ];
 
